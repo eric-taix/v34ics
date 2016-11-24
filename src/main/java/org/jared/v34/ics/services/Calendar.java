0fc41db1106cfd6ead5b49c9f7d51bc3813b3f0a
Eric Taix <eric.taix@itk.fr>
2016-11-24 23:29:31 +0100

    Use global timezone ID

--- src/main/java/org/jared/v34/ics/services/Calendar.java
+++ src/main/java/org/jared/v34/ics/services/Calendar.java
@@ -1,10 +1,11 @@
 package org.jared.v34.ics.services;
 
+import org.jared.v34.ics.exception.V34Exception;
 import org.jared.v34.ics.model.Event;
 
 import java.util.List;
 
 public interface Calendar {
 
-    String getCalendar(List<Event> events);
+    String getCalendar(List<Event> events) throws V34Exception;
 }
