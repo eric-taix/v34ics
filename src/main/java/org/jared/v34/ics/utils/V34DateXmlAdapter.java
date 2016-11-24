cd4714119af0e808bc8a7b4184004e43c638a798
Eric Taix <eric.taix@itk.fr>
2016-11-24 01:32:31 +0100

    Fix event date timezone

--- src/main/java/org/jared/v34/ics/utils/V34DateXmlAdapter.java
+++ src/main/java/org/jared/v34/ics/utils/V34DateXmlAdapter.java
@@ -3,18 +3,21 @@ package org.jared.v34.ics.utils;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class V34DateXmlAdapter extends XmlAdapter<String, Date> {
 
-    private SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-
     @Override
     public Date unmarshal(String v) throws Exception {
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        format.setTimeZone(TimeZone.getTimeZone("Europe/Paris"));
         return format.parse(v);
     }
 
     @Override
     public String marshal(Date v) throws Exception {
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        format.setTimeZone(TimeZone.getTimeZone("Europe/Paris"));
         return format.format(v);
     }
 }
